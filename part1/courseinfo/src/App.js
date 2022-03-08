3a42211486e080970a44d979ef642d7301a860e7
Debabrata Ghosh <rijugh@gmail.com>
2022-03-08 10:49:03 +0530

    Part 1 question 1 answer added
    course information, step 1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,22 +2,20 @@ function Header(props) {
   return <h1>{props.course}</h1>;
 }
 
-function Part(props) {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-}
-
 function Content(props) {
-  return (
-    <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </div>
-  );
+  return [
+    <p>
+      {props.part1} {props.exercises1}
+    </p>,
+
+    <p>
+      {props.part2} {props.exercises2}
+    </p>,
+
+    <p>
+      {props.part3} {props.exercises3}
+    </p>,
+  ];
 }
 
 function Total(props) {
@@ -31,15 +29,23 @@ function Total(props) {
 
 function App() {
   const course = "Half Stack application development";
+
   const part1 = "Fundamentals of React";
+
   const exercises1 = 10;
+
   const part2 = "Using props to pass data";
+
   const exercises2 = 7;
+
   const part3 = "State of a component";
+
   const exercises3 = 14;
+
   return (
     <div>
       <Header course={course} />
+
       <Content
         part1={part1}
         part2={part2}
@@ -48,6 +54,7 @@ function App() {
         exercises2={exercises2}
         exercises3={exercises3}
       />
+
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
