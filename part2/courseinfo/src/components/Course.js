fcb4b2c12647e2700d316db6f453a5f7859febd1
Debabrata Ghosh <rijugh@gmail.com>
2022-03-15 10:16:41 +0530

    Part 2 question 4 & 5 answer added
    Course information step9 with separate module

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,12 +2,23 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 
-const Course = ({ course }) => {
+const Single = ({ course }) => {
   return (
     <div>
-      <Header key={course.id} course={course} />
+      <Header course={course} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Course = ({ courses }) => {
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <Single key={course.id} course={course} />
+      ))}
     </div>
   );
 };
