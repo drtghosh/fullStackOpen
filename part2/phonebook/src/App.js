8f64c459a8c3b1a379b710003275bcde4e682162
Debabrata Ghosh <rijugh@gmail.com>
2022-03-16 12:32:10 +0530

    Part 2 question 9 answer added
    The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,15 +3,29 @@ import Numbers from "./components/Numbers";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const personsToShow =
+    filter.length === 0
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        );
+
   const changeName = (event) => {
     setNewName(event.target.value);
   };
   const changeNumber = (event) => {
     setNewNumber(event.target.value);
+  };
+  const changeFilter = (event) => {
+    setFilter(event.target.value);
   };
   const addName = (event) => {
     event.preventDefault();
@@ -30,6 +44,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={changeFilter} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input onChange={changeName} />
@@ -45,7 +63,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        <Numbers numbers={persons} />
+        <Numbers numbers={personsToShow} />
       </div>
     </div>
   );
